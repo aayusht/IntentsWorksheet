7b622ebebea2ca4fd0386fb4afd7165e1a05f7dd
Aayush <ayushtyagi@gmail.com>
2017-02-06 14:43:02 -0800

    wrote solutions

--- app/src/main/java/com/demo/mdb/intentsworksheet/MainActivity.java
+++ app/src/main/java/com/demo/mdb/intentsworksheet/MainActivity.java
@@ -1,8 +1,14 @@
 package com.demo.mdb.intentsworksheet;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,20 +16,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Part 1: These five questions will create an Intent that will open the OtherActivity when
-        // a button is pressed.
-        //Question 1: create a button in the layout file. Do not delete the "Hello World" TextView.
-        //Question 2: create the Button object below. How do you find it tho? if only there was a
         // way to find a view by its ID...
 
-        /* your code here */
+        Button button = (Button) findViewById(R.id.button);
 
         //Question 3: find a way for the Button to LISTEN ON the CLICKs. Use Google for guidance.
         //Question 4: every time the button is clicked, create and execute an Intent to go to the
         // OtherClass.
 
-        /* your code here */
-
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), OtherActivity.class);
+                startActivity(intent);
+            }
+        });
 
         //Part 3: create an Intent that has a button that opens the camera app, takes a picture, and
         // displays the picture. Note: this requires a line in the manifest, but that comes later,
@@ -32,8 +39,15 @@ public class MainActivity extends AppCompatActivity {
         //Question 10: Create the button in the layout and add the listener with the Intent. What
         // does the Intent look like? If you're not opening a class, then what do you pass in?
         // Google it bb
-
-        /* your code here */
+        ((Button) findViewById(R.id.button3)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(takePictureIntent, 1);
+                }
+            }
+        });
     }
 
     @Override
@@ -41,27 +55,33 @@ public class MainActivity extends AppCompatActivity {
         super.onResume();
         //Question 9: Every time this Activity is resumed, set the text of the TextView to the extra
         // value from the intent you got from OtherActivity.
-
-        /* your code here */
+        String newString = getIntent().getStringExtra("textinput");
+        if (newString != null) {
+            ((TextView) findViewById(R.id.textView)).setText(newString);
+        }
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        //Question 11: retrieve the result from the intent. Do not use get Intent, use the parameter
-        // data.
 
-        /* your code here */
+        if (requestCode == 1 && resultCode == RESULT_OK) {
 
-        //Question 12: Create a Bitmap object and set it to the data from the Intent. Use the key
-        // "data" to retrieve the Bitmap.
+            //Question 11: retrieve the result from the intent. Do not use get Intent, use the parameter
+            // data.
 
-        /* your code here */
+            Bundle extras = data.getExtras();
 
-        //Question 13: create an imageView in the layout
-        //Question 14: set the imageView to the Bitmap
+            //Question 12: Create a Bitmap object and set it to the data from the Intent. Use the key
+            // "data" to retrieve the Bitmap.
 
-        /* your code here */
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+
+            //Question 13: create an imageView in the layout
+            //Question 14: set the imageView to the Bitmap
+
+            ((ImageView) findViewById(R.id.imageView)).setImageBitmap(imageBitmap);
+        }
 
         //ye
     }
--- app/src/main/java/com/demo/mdb/intentsworksheet/OtherActivity.java
+++ app/src/main/java/com/demo/mdb/intentsworksheet/OtherActivity.java
@@ -1,7 +1,11 @@
 package com.demo.mdb.intentsworksheet;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class OtherActivity extends AppCompatActivity {
 
@@ -18,13 +22,21 @@ public class OtherActivity extends AppCompatActivity {
         //Question 6: Add an EditText and Button in the layout files, and add the EditText and
         // Button objects below.
 
-        /* your code here */
+        final EditText editText = (EditText) findViewById(R.id.editText);
+        Button button = (Button) findViewById(R.id.button2);
 
         //Question 7: Add the listener to the Button you instantiated.
         //Question 8: Every time the button is clicked, create the intent, get the text from the
         // edit text, put it in the intent, and execute the intent.
 
-        /* your code below */
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                intent.putExtra("textinput", editText.getText().toString());
+                startActivity(intent);
+            }
+        });
 
         //Question 9 is in MainActivity
     }
